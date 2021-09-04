478f8fcd1d3e4f258b8cdeaac8a4420afeb81c98
NicolasAraya932 <63214589+NicolasAraya932@users.noreply.github.com>
2021-09-03 21:04:15 -0400

    Hola

---
+++ main.py
@@ -0,0 +1,14 @@
+import flask as f
+from flask_ngrok import run_with_ngrok
+
+app = f.Flask(__name__)
+run_with_ngrok(app)
+
+@app.route('/')
+def inicio():
+    return f.render_template("intentodescript.html")
+
+
+if __name__ == "__main__":
+    app.config['DEBUG'] = True
+    app.run()
